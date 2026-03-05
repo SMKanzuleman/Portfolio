36e67aba6b7ff9161b7ff1b549bbc4c70e03ea1f
SMKanzulEman <syedkanzuleman.official@gmail.com>
2026-03-06 01:21:59 +0500

    Add internal Links inside Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,25 @@ import MySkills from "./components/MySkills/MySkills";
 import SkillCard from "./components/MySkills/SkillCard";
 import Project from "./components/Projects/Project";
 import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="bg-[#0a0a0a] h-screen text-white">
       <Navbar className="fixed top-0 z-50" />
       <HeroSection />
-      <About />
-      <MySkills />
-      <Project />
-      <Contact />
+      <section id="about">
+        <About />
+      </section>
+      <section id="myskills">
+        <MySkills />
+      </section>
+      <section id="projects">
+        <Project />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
       <Footer />
     </div>
   );
--- src/components/Navbarr/Navbar.jsx
+++ src/components/Navbarr/Navbar.jsx
@@ -4,10 +4,11 @@ export default function Navbar() {
     <div className=" fixed top-7 w-full flex items-center justify-center z-100">
       <div className="w-1/3 h-15 rounded-full bg-linear-to-r from-purple-600 to-purple-800  px-7 py-4 transition-all duration-500 animate-float-breath">
         <div className="w-full h-full flex justify-center gap-7 py-0 items-center list-none cursor-pointer">
-          <NavItem data="Home" />
-          <NavItem data="About" />
-          <NavItem data="Projects" />
-          <NavItem data="Contact" />
+          <NavItem data="Home" link="#" />
+          <NavItem data="About" link="#about" />
+          <NavItem data="Projects" link="#projects" />
+          <NavItem data="Skills" link="#myskills" />
+          <NavItem data="Contact" link="#contact" />
         </div>
       </div>
     </div>

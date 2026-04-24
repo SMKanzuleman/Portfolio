38de682a7ae2d1a720f85402c1ccf0eb88952cec
Syed KanzulEman <syedkanzuleman.official@gmail.com>
2026-04-24 14:54:35 +0500

    Refactor Gbtn component to improve button rendering logic

--- src/components/Global/Gbtn.jsx
+++ src/components/Global/Gbtn.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 const Gbtn = (props) => {
+  if (props.onClick) {
+    return (
+      <button
+        onClick={props.onClick}
+        className="font-Outfit px-7 py-3 rounded-lg bg-linear-to-r from-purple-700 to-purple-800 font-semibold hover:scale-105 transition-all duration-250 ease-in-out cursor-pointer inline-block hover:text-black"
+      >
+        {props.data}
+      </button>
+    );
+  }
   return (
     <a
       href={props.url}
